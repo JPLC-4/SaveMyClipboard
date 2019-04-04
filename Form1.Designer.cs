8a4e515ae195828c14de1d0cebf50c34f38e8c47
JPLC-4 <49209232+JPLC-4@users.noreply.github.com>
2019-04-04 02:11:46 +0200

    Fichier du projet

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace SaveMyClipboard
+{
+    partial class Programme
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.Checkbox = new System.Windows.Forms.CheckBox();
+            this.Boutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(74, 13);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(129, 13);
+            this.label.TabIndex = 1;
+            this.label.Text = "Mode Automatique Activé";
+            // 
+            // Checkbox
+            // 
+            this.Checkbox.AutoSize = true;
+            this.Checkbox.Checked = true;
+            this.Checkbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.Checkbox.Location = new System.Drawing.Point(53, 12);
+            this.Checkbox.Name = "Checkbox";
+            this.Checkbox.Size = new System.Drawing.Size(15, 14);
+            this.Checkbox.TabIndex = 2;
+            this.Checkbox.UseVisualStyleBackColor = true;
+            // 
+            // Boutton
+            // 
+            this.Boutton.Location = new System.Drawing.Point(77, 45);
+            this.Boutton.Name = "Boutton";
+            this.Boutton.Size = new System.Drawing.Size(75, 23);
+            this.Boutton.TabIndex = 4;
+            this.Boutton.Text = "Lancer";
+            this.Boutton.UseVisualStyleBackColor = true;
+            this.Boutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Programme
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 80);
+            this.Controls.Add(this.Boutton);
+            this.Controls.Add(this.Checkbox);
+            this.Controls.Add(this.label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Programme";
+            this.Text = "SaveMyClipboard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.CheckBox Checkbox;
+        private System.Windows.Forms.Button Boutton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+using System.IO;
+
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaveMyClipboard
+{
+    public partial class Programme : Form
+    {
+        public Programme()
+        {
+            InitializeComponent();
+        }
+        public string TexteDuPressePapier = string.Empty;
+        string Fichier = Path.GetTempPath() + "//" + "SaveMyClipboard" + ".txt";
+        string Date = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss tt");
+
+
+        private void EcrireTexteduPressepapier()
+        {
+            if (Checkbox.Checked == true)
+            {
+                while (true)
+                {
+                    Task.Delay(3000);
+                    string Texte = Clipboard.GetText();
+                    
+                    if (Texte != TexteDuPressePapier)
+                    {
+                        if (Texte != string.Empty)
+                        {
+                            File.AppendAllText(Fichier, Date + Environment.NewLine + Texte + Environment.NewLine);
+                            TexteDuPressePapier = Texte;
+                        }
+                        
+                    }
+                    
+                   
+                }
+            }
+
+            else
+            {
+                label.Text = "Mode Automatique Desactivé";
+            }
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EcrireTexteduPressepapier();
+           
+        }
+    }
+}
